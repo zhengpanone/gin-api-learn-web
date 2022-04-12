c7ca595a479b4e84350d2be73df1547e83abdd6c
zhengpanone <zhengpanone@hotmail.com>
2022-04-12 23:41:17 +0800

    chore: 配置eslint验证规则

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-strongly-recommended', // 修改为vue3 的eslint验证规则
+    'standard'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 npm init vite@latest
 
 cd gin-api-learn-web && npm run dev
+
+## eslint
+
+npm install eslint -D
+npx eslint --init

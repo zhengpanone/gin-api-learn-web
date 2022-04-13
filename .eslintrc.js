1604b707d65dbd075fab4594bbb14006bfdd34ef
zhengpanone <zhengpanone@hotmail.com>
2022-04-13 22:51:56 +0800

    fix: set-up编译宏

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,18 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-strongly-recommended', // 修改为vue3 的eslint验证规则
     'standard'],
+
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {}
+  rules: {},
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  }
 }

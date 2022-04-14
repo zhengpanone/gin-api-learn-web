62f4d4ae4d7a85b8878d9b8b34a515aaaa8dc87d
zhengpanone <zhengpanone@hotmail.com>
2022-04-14 22:15:00 +0800

    feat: 引入pinia

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 
 <template>
+  <div>{{ mainStore.name }}</div>
   <!-- 路由渲染出口 -->
   <router-view />
 </template>
 <script setup lang="ts">
+import { useMainStore } from '@/store/index'
+const mainStore = useMainStore()
 </script>
 <style>
 
